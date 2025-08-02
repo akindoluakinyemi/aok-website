5693c1a700df05bbe3bd8b40aebc9b3f9305bccc
ebube <ebubeokafor02@gmail.com>
2025-08-02 22:59:06 +0100

    changed general font of the page and adjusted issues with medium layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        castoro: ["Castoro", "cursive"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
